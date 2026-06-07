bb479c666090d576f33f3b73b37ff63eea551b37
Zoroiscrying <zoroiscrying@outlook.com>
2026-06-07 13:12:47 +0800

    debug: check [connection] status in SettingsMenuScreen
    Added prints in _on_enter:
    - BackButton reference
    - BackButton pressed signal connections list
    - _on_back_pressed call trace
    
    This will show if [connection] was applied and if the signal fires.

--- addons/ui_flow/examples/main_menu_flow/screens/settings_menu_screen.gd
+++ addons/ui_flow/examples/main_menu_flow/screens/settings_menu_screen.gd
@@ -1,20 +1,17 @@
 ## Settings menu placeholder - shows navigation depth.
 class_name SettingsMenuScreen extends UIFlowPage
 
-@onready var _back_button: Button = $Center/BackButton
-
-
-func _ready() -> void:
-	_back_button.pressed.connect(_on_back_pressed)
-
 
 func _on_back_pressed() -> void:
+	print("SettingsMenuScreen: _on_back_pressed called!")
 	UIFlow.pop()
 
 
 func _on_enter(_data: Dictionary = {}) -> void:
 	$Center/DepthLabel.text = "Stack depth: %d" % UIFlow.stack_depth()
 	UIFlow.set_default_focus($Center/BackButton)
+	print("SettingsMenuScreen: _on_enter, BackButton = ", $Center/BackButton)
+	print("SettingsMenuScreen: BackButton pressed signal connections: ", $Center/BackButton.pressed.get_connections())
 
 
 func _on_resume() -> void:
