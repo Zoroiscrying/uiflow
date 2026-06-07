e4f9d4467ef48bcc531b633bbd4acc79baf92e74
Zoroiscrying <zoroiscrying@outlook.com>
2026-06-07 13:50:21 +0800

    debug: comprehensive diagnostics for SettingsMenuScreen
    _ready: prints node name, BackButton reference, signal connections
    _on_enter: prints when called
    _on_back_pressed: prints when button pressed
    
    [connection] in .tscn: Center/BackButton.pressed -> _on_back_pressed

--- addons/ui_flow/examples/main_menu_flow/screens/settings_menu_screen.gd
+++ addons/ui_flow/examples/main_menu_flow/screens/settings_menu_screen.gd
@@ -1,17 +1,23 @@
 ## Settings menu placeholder - shows navigation depth.
+## BackButton connected via Inspector signal in .tscn.
 class_name SettingsMenuScreen extends UIFlowPage
 
 
+func _ready() -> void:
+	print("SettingsMenuScreen: _ready called, node = ", name)
+	print("SettingsMenuScreen: BackButton = ", $Center/BackButton)
+	print("SettingsMenuScreen: pressed connections = ", $Center/BackButton.pressed.get_connections())
+
+
 func _on_back_pressed() -> void:
-	print("SettingsMenuScreen: _on_back_pressed called!")
+	print("SettingsMenuScreen: BACK PRESSED!")
 	UIFlow.pop()
 
 
 func _on_enter(_data: Dictionary = {}) -> void:
+	print("SettingsMenuScreen: _on_enter called")
 	$Center/DepthLabel.text = "Stack depth: %d" % UIFlow.stack_depth()
 	UIFlow.set_default_focus($Center/BackButton)
-	print("SettingsMenuScreen: _on_enter, BackButton = ", $Center/BackButton)
-	print("SettingsMenuScreen: BackButton pressed signal connections: ", $Center/BackButton.pressed.get_connections())
 
 
 func _on_resume() -> void:
